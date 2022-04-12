f90e6d9f522d48b6657bab6a38e70b21c16ff16d
qubit4 <82866970+qubit4@users.noreply.github.com>
2022-04-12 17:45:12 +0200

    Create urls.js

---
+++ src/urls.js
@@ -0,0 +1,3 @@
+export const url_posts = "https://jsonplaceholder.typicode.com/posts"
+export const url_users = "https://jsonplaceholder.typicode.com/users"
+export const url_comments = "https://jsonplaceholder.typicode.com/comments"
